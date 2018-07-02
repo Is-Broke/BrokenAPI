a8b2866bce1997eb5dbc33f15dbe7235a7facc25
paulritzman <ritzman.paul@gmail.com>
2018-07-02 12:59:51 -0700

    Started intial buildout of ErrorCategory and Error Classes

--- BrokenApi/BrokenApi/Controllers/ErrorCategoryController.cs
+++ BrokenApi/BrokenApi/Controllers/ErrorCategoryController.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BrokenApi.Data;
+using BrokenApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BrokenApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/ErrorCategory")]
     [ApiController]
     public class ErrorCategoryController : ControllerBase
     {
+        private BrokenAPIContext _context;
 
+        public ErrorCategoryController(BrokenAPIContext context)
+        {
+            _context = context;
+
+            if (_context.Categories.Count() == 0)
+            {
+                _context.Categories.Add(new ErrorCategory { Name = "Test Error Category" });
+                _context.SaveChanges();
+            }
+        }
     }
 }
