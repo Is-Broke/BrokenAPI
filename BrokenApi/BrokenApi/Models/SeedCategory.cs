9ce04e4da6247f39b20837faac9cf4980ba534d1
Judah Hunger <judah.hunger@gmail.com>
2018-07-03 14:22:25 -0700

    updated Category seed file and fixed curly bracket on Category controller

--- BrokenApi/BrokenApi/Models/SeedCategory.cs
+++ BrokenApi/BrokenApi/Models/SeedCategory.cs
@@ -24,17 +24,17 @@ namespace BrokenApi.Models
                     new ErrorCategory
                     {
                         ErrorType = "Logic",
-                        Description = "Logic Error",
+                        Description = "Logic errors are those that appear once the application is in use. They are most often unwanted or unexpected results in response to user actions. For example, a mistyped key or other outside influence might cause your application to stop working within expected parameters, or altogether. Logic errors are generally the hardest type to fix, since it is not always clear where they originate.",
                     },
                     new ErrorCategory
                     {
                         ErrorType = "Runtime",
-                        Description = "Runtime Error",
+                        Description = "Run-time errors are those that appear only after you compile and run your code. These involve code that may appear to be correct in that it has no syntax errors, but that will not execute. For example, you might correctly write a line of code to open a file. But if the file is corrupted, the application cannot carry out the Open function, and it stops running. You can fix most run-time errors by rewriting the faulty code, and then recompiling and rerunning it",
                     },
                     new ErrorCategory
                     {
                         ErrorType = "Syntax",
-                        Description = "Syntax Error",
+                        Description = "Syntax errors are those that appear while you write code. Visual Basic checks your code as you type it in the Code Editor window and alerts you if you make a mistake, such as misspelling a word or using a language element improperly. Syntax errors are the most common type of errors. You can fix them easily in the coding environment as soon as they occur.",
                     });
 
                 await context.SaveChangesAsync();
