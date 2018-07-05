fbcca2322108e281fc4d5a8954fac5460e299434
Judah Hunger <judah.hunger@gmail.com>
2018-07-05 12:45:35 -0700

    added more seed data to error seed file

--- BrokenApi/BrokenApi/Models/SeedError.cs
+++ BrokenApi/BrokenApi/Models/SeedError.cs
@@ -25,21 +25,21 @@ namespace BrokenApi.Models
                     {
                         ErrorCategoryID = 0,
                         DetailedName = "Unexpected Output string",
-                        Link = "",
+                        Link = "https://docs.microsoft.com/en-us/dotnet/visual-basic/programming-guide/language-features/error-types",
                         CodeExample = $"public static void Main(string[] args)" +
-        $"{{" +
-            $"Console.WriteLine(\"Hello World\");" +
-            $"string firstName = \"John\";" +
-            $"string lastName = \"Doe\";" +
-            $"MakeFullName(firstName, lastName);" +
-        $"}}" +
-        $"public static void MakeFullName(string one, string two)" +
-        $"{{" +
-            $"Console.WriteLine(one + two);  // output is JohnDoe" +
+                        $"{{" +
+                        $"Console.WriteLine(\"Hello World\");" +
+                        $"string firstName = \"John\";" +
+                        $"string lastName = \"Doe\";" +
+                        $"MakeFullName(firstName, lastName);" +
+                        $"}}" +
+                        $"public static void MakeFullName(string one, string two)" +
+                        $"{{" +
+                        $"Console.WriteLine(one + two);  // output is JohnDoe" +
 
-            $"Console.WriteLine(one + \" \" + two); //Correct output of John Doe" +
-            $"Console.WriteLine($\"{{one}} {{two}}\"); // same as above" +
-        $"}}",
+                        $"Console.WriteLine(one + \" \" + two); //Correct output of John Doe" +
+                        $"Console.WriteLine($\"{{one}} {{two}}\"); // same as above" +
+                        $"}}",
                         IsUserExample = false,
                         Votes = 0,
                         Rating = 0,
@@ -50,23 +50,23 @@ namespace BrokenApi.Models
                      {
                          ErrorCategoryID = 0,
                          DetailedName = "Unexpected Output Int",
-                         Link = "",
+                         Link = "https://docs.microsoft.com/en-us/dotnet/visual-basic/programming-guide/language-features/error-types",
                          CodeExample = $"public static void Main(string[] args)" +
-        $"{{" +
-            $"Console.WriteLine(\"Hello World\");" +
-           $"Console.WriteLine(FindAverage(20, 12));" +
-            $"Console.WriteLine(FindAverageTrue(20, 12));" +
-        $"}}" +
-        $"public static int FindAverage(int x, int y)" +
-       $"{{" +
-            $"return x + y / 2;   // return 26 wrong output for average wanted 16" +
-        $"}}" +
+                         $"{{" +
+                         $"Console.WriteLine(\"Hello World\");" +
+                         $"Console.WriteLine(FindAverage(20, 12));" +
+                         $"Console.WriteLine(FindAverageTrue(20, 12));" +
+                         $"}}" +
+                         $"public static int FindAverage(int x, int y)" +
+                         $"{{" +
+                         $"return x + y / 2;   // return 26 wrong output for average wanted 16" +
+                         $"}}" +
 
-        $"public static int FindAverageTrue(int x, int y)" +
-        $"{{" +
-            $"return (x + y) / 2;  // returned the correct output for average" +
-        $"}}" +
-    $"}}",
+                         $"public static int FindAverageTrue(int x, int y)" +
+                         $"{{" +
+                         $"return (x + y) / 2;  // returned the correct output for average" +
+                         $"}}" +
+                         $"}}",
                          IsUserExample = false,
                          Votes = 0,
                          Rating = 0,
@@ -88,7 +88,7 @@ namespace BrokenApi.Models
                         "{\n" +
                         "\tNode formerTop = inputStack.Pop();\n" +
                         "\treturn formerTop;\n" +
-                        "}",    
+                        "}",
                         IsUserExample = false,
                         Votes = 0,
                         Rating = 0,
@@ -114,6 +114,101 @@ namespace BrokenApi.Models
                         DetailedName = "Invalid Conversion",
                         Votes = 0,
                         Rating = 0,
+                    },
+                    new Error
+                    {
+                        ErrorCategoryID = 2,
+                        CodeExample = " b = (4 + 6;  // missing closing parenthesis, ')' expected",
+                        DetailedName = "Missing closing parenthesis",
+                        Votes = 0,
+                        Rating = 0,
+                        Description = "forgot to close parenthesis",
+                        IsUserExample = false,
+                        Link = ""
+                    },
+                    new Error
+                    {
+                        ErrorCategoryID = 2,
+                        CodeExample = " b = 5 + * 9;   // missing argument between + and *",
+                        DetailedName = "missing argument",
+                        Votes = 0,
+                        Rating = 0,
+                        Description = "Missing something between the addition and multiply signs.",
+                        IsUserExample = false,
+                        Link = "https://msdn.microsoft.com/en-us/library/system.argumentexception(v=vs.110).aspx"
+                    },
+                    new Error
+                    {
+                        ErrorCategoryID = 2,
+                        CodeExample = $"int i; \n" +
+                        $"i++    // use of unassigned local variable 'i' ",
+                        DetailedName = "not initialized",
+                        Votes = 0,
+                        Rating = 0,
+                        Description = "Semantic Error where variable is not initialized",
+                        IsUserExample = false,
+                        Link = ""
+                    },
+                    new Error
+                    {
+                        ErrorCategoryID = 2,
+                        CodeExample = "int a = \"John\";  // Cannot implicitly convert type 'string' to 'int'",
+                        DetailedName = "Type incompatibility",
+                        Votes = 0,
+                        Rating = 0,
+                        Description = "the types string and int are not compatible",
+                        IsUserExample = false,
+                        Link = "https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/casting-and-type-conversions",
+                    },
+                    new Error
+                    {
+                        ErrorCategoryID = 1,
+                        CodeExample = $"int[] myarray = new int[10];\n" +
+                         $" myarray[5] = 5; \n" +
+                         $" myarray[10] = 10;\n" +
+                         $"Console.WriteLine(myarray);",
+                        DetailedName = "System.IndexOutOfRangeException",
+                        Votes = 0,
+                        Rating = 0,
+                        Description = " 'Index was outside the bounds of the array.'",
+                        IsUserExample = false,
+                        Link = "https://docs.microsoft.com/en-us/dotnet/api/system.indexoutofrangeexception?view=netframework-4.7.1"
+                    },
+                    new Error
+                    {
+                        ErrorCategoryID = 0,
+                        CodeExample = $"string x = Console.ReadLine(); \n" +
+                        $"while (x != null){{\n" +
+                        $"Console.WriteLine(x);\n" +
+                        $"}}",
+                        DetailedName = "Loop doesn't Terminate",
+                        Votes = 0,
+                        Rating = 0,
+                        Description = "Constantly prints to console with loop that doesn't end.",
+                        IsUserExample = false,
+                        Link = "https://docs.microsoft.com/en-us/dotnet/visual-basic/programming-guide/language-features/error-types"
+                    },
+                    new Error
+                    {
+                        ErrorCategoryID = 1,
+                        CodeExample = "using (StreamReader sr = new StreamReader(path: \"../somthing.txt\"));",
+                        DetailedName = "System.IO.FileNotFoundException",
+                        Votes = 0,
+                        Rating = 0,
+                        Description = "if file is not on in path throws file not found",
+                        IsUserExample = false,
+                        Link = "https://docs.microsoft.com/en-us/dotnet/api/system.io.filenotfoundexception?view=netframework-4.7.1"
+                    },
+                    new Error
+                    {
+                        ErrorCategoryID = 2,
+                        CodeExample = "int a = 5   // semicolon is missing, ';' expected",
+                        DetailedName = "Missing semicolon",
+                        Votes = 0,
+                        Rating = 0,
+                        Description = "Forgetting to put semicolon after statement",
+                        IsUserExample = false,
+                        Link = ""  
                     });
 
                 await context.SaveChangesAsync();
