e9db25b56185f5dc2c2feb105398936b85374fa6
paulritzman <ritzman.paul@gmail.com>
2018-07-05 12:45:49 -0700

    Completed GetTop() tests - all pass

--- BrokenApi/ErrorController-Unit-Tests/UnitTest1.cs
+++ BrokenApi/ErrorController-Unit-Tests/UnitTest1.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 using BrokenApi.Data;
 using BrokenApi.Models;
 using BrokenApi.Controllers;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace ErrorController_Unit_Tests
 {
@@ -78,7 +80,60 @@ namespace ErrorController_Unit_Tests
                 Assert.Equal(1, results.Count());
             }
         }
-        
+
+        [Theory]
+        [InlineData("firstTestError", 1)]
+        [InlineData("secondTestError", 5)]
+        [InlineData("thirdTestError", 10)]
+        public async void GetTopCanReturnTheErrorWithTheMostUpVotes(string errName, int numVotes)
+        {
+            DbContextOptions<BrokenAPIContext> options =
+                new DbContextOptionsBuilder<BrokenAPIContext>()
+                .UseInMemoryDatabase(errName)
+                .Options;
+
+            using (BrokenAPIContext context = new BrokenAPIContext(options))
+            {
+                // Arrange
+                ErrorController ec = new ErrorController(context);
+
+                Error controlError = new Error
+                {
+                    ErrorCategoryID = 0,
+                    DetailedName = "controlError",
+                    Description = "This is a test of the Post method.",
+                    Link = "test",
+                    CodeExample = "test",
+                    IsUserExample = false,
+                    Votes = 0,
+                    Rating = 0
+                };
+
+                Error testError = new Error
+                {
+                    ErrorCategoryID = 0,
+                    DetailedName = errName,
+                    Description = "This is a test of the Post method.",
+                    Link = "test",
+                    CodeExample = "test",
+                    IsUserExample = false,
+                    Votes = numVotes,
+                    Rating = 0
+                };
+
+                await context.Errors.AddAsync(controlError);
+                await context.Errors.AddAsync(testError);
+                await context.SaveChangesAsync();
+
+                // Act
+                var topError = ec.GetTop();
+
+                // Assert
+                Assert.Equal(errName, topError.Result.Value.DetailedName);
+            }
+        }
+
+
 
     }
 }
